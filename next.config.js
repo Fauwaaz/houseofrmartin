e4e45e6dd2dc9f79d731cfdec9151427cd00ae02
DomVournias <domvournias@gmail.com>
2022-10-20 01:40:19 +0300

    Whitelisted domain for images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["nextecomwp.tk"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
