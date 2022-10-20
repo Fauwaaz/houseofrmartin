d954a192d0564d093a6643e96562f420c0cee3f1
DomVournias <domvournias@gmail.com>
2022-10-20 18:32:52 +0300

    Context Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { ApolloProvider } from "@apollo/client";
 import client from "../libs/apollo";
 import "../styles/globals.css";
+import { StateContext } from "../context/StateContext";
 
 function App({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
-      <Component {...pageProps} />
+      <StateContext>
+        <Component {...pageProps} />
+      </StateContext>
     </ApolloProvider>
   );
 }
