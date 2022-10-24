b034d95ce357a5854380e900299b1ee23e883f2e
DomVournias <domvournias@gmail.com>
2022-10-24 21:30:46 +0300

    Banner

---
+++ components/Banner.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { RiArrowRightSLine } from "react-icons/ri";
+import styles from "../styles/Banner.module.css";
+
+const Banner = ({ title, description, uri, image }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <Link href={`/product/${uri}`}>
+          <a>
+            Learn more <RiArrowRightSLine />
+          </a>
+        </Link>
+      </div>
+      <div className={styles.image}>
+        <Image src={image} width="746px" height="388px" objectFit="contain" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
